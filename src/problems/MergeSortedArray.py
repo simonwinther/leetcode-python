5983cc832ad280d0e62cfa011c5b8268aca05b89
Simon Winther <simonwinther22@gmail.com>
2022-03-01 19:08:51 +0100

    Added MergeSortedArray.py

---
+++ src/problems/MergeSortedArray.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    p1 = m - 1
+    p2 = n - 1
+
+    for p in range(n + m - 1, -1, -1):
+        if p2 < 0:
+            break
+        if p1 >= 0 and nums1[p1] > nums2[p2]:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+
+
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+merge(nums1, m, nums2, n)
+print(nums1)
