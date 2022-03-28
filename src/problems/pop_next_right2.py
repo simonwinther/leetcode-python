4adffcbae305ca08697ae373b7012141e164bf08
Simon Winther <simonwinther22@gmail.com>
2022-03-28 20:58:17 +0200

    Changed project structure and added pop_next_right2.py

---
+++ src/problems/pop_next_right2.py
@@ -0,0 +1,18 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+def connect(self, root: 'Node') -> 'Node':
+    if not root:
+        return root
+    queue = [root]
+    while queue:
+        for i in range(len(queue) - 1):
+            queue[i].next = queue[i + 1]
+        queue = [child for node in queue for child in (node.left, node.right) if child]
+    return root
