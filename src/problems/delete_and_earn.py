577a7973d68210b02350be8c86951fcedf2f1412
Simon Winther <simonwinther22@gmail.com>
2022-04-25 18:27:59 +0200

    Added delete_and_earn.py

---
+++ src/problems/delete_and_earn.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def deleteAndEarn(nums: List[int]) -> int:
+    if not nums:
+        return 0
+
+    freq = [0] * (max(nums) + 1)
+
+    for n in nums:
+        freq[n] += n
+
+    dp = [0] * len(freq)
+    dp[1] = freq[1]
+
+    for i in range(2, len(freq)):
+        dp[i] = max(freq[i] + dp[i - 2], dp[i - 1])
+
+    return dp[len(freq) - 1]
+
+
+print(deleteAndEarn(nums=[3, 4, 2]))
+print(deleteAndEarn(nums=[2, 2, 3, 3, 3, 4]))
