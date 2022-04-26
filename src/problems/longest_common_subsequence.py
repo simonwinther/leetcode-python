d0e014d0d6356cbfdbecfda604b5b667d28b7b3c
Simon Winther <simonwinther22@gmail.com>
2022-04-26 18:19:31 +0200

    Added longest_common_subsequence.py

---
+++ src/problems/longest_common_subsequence.py
@@ -0,0 +1,17 @@
+def longestCommonSubsequence(text1: str, text2: str) -> int:
+    n, m = len(text1), len(text2)
+
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    for i in range(n):
+        for j in range(m):
+            if text1[i] == text2[j]:
+                dp[i + 1][j + 1] = 1 + dp[i][j]
+            else:
+                dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+    return dp[-1][-1]
+
+
+print(longestCommonSubsequence(text1="abcde", text2="ace"))
+print(longestCommonSubsequence(text1="abc", text2="abc"))
+print(longestCommonSubsequence(text1="abc", text2="def"))
