25de4bd015baad927da591917b0741cee31514b4
Simon Winther <simonwinther22@gmail.com>
2022-07-16 22:22:55 +0200

    Added reverse_k_sublist.py and reverse_sublist.py

---
+++ src/problems/reverse_sublist.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+
+def reverse_sub_list(head, p, q):
+    if p == q:
+        return head
+    current, previous = head, None
+    i = 0
+    while current is not None and i < p - 1:
+        previous = current
+        current = current.next
+        i += 1
+
+    last_node_of_first_part = previous
+    last_node_of_sub_list = current
+    next = None
+
+    i = 0
+    while current is not None and i < q - p + 1:
+        next = current.next
+        current.next = previous
+        previous = current
+        current = next
+        i += 1
+
+    if last_node_of_first_part is not None:
+        last_node_of_first_part.next = previous
+    else:
+        head = previous
+    last_node_of_sub_list.next = current
+    return head
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+
+    print("Nodes of original LinkedList are: ", end="")
+    head.print_list()
+    result = reverse_sub_list(head, 2, 4)
+    print("Nodes of reversed LinkedList are: ", end="")
+    result.print_list()
+
+
+main()
